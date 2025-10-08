c67983e91a704ce03143363b7d8e7b4f75e57f5e
taki-deen <57011692+taki-deen@users.noreply.github.com>
2025-10-08 12:21:24 +0300

    Fix TypeScript ref error in VerifyOTPPage

--- client/src/pages/VerifyOTPPage.tsx
+++ client/src/pages/VerifyOTPPage.tsx
@@ -163,7 +163,9 @@ const VerifyOTPPage: React.FC = () => {
               {otp.map((digit, index) => (
                 <input
                   key={index}
-                  ref={(el) => (inputRefs.current[index] = el)}
+                  ref={(el) => {
+                    inputRefs.current[index] = el;
+                  }}
                   type="text"
                   inputMode="numeric"
                   maxLength={1}
